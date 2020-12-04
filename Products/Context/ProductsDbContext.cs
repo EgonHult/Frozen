e8cfe163e5043eb0fa6f3ea92465c41cf1f39eb3
VictorJernestrand <56867809+VictorJernestrand@users.noreply.github.com>
2020-12-04 15:02:19 +0100

    Products db initialcreate, context create

--- Products/Context/ProductsDbContext.cs
+++ Products/Context/ProductsDbContext.cs
@@ -21,6 +21,13 @@ namespace Products.Context
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.Entity<ProductsModel>(entity => {
+                entity.Property(x => x.Name).IsRequired();
+                entity.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");
+                entity.Property(x => x.Quantity).IsRequired();
+                entity.Property(x => x.Image).HasDefaultValue("https://i.pinimg.com/originals/21/ff/a1/21ffa154e3d8639299017ab5683e55cc.jpg");
+                entity.Property(x => x.Details).HasDefaultValue("Information om produkten saknas...");
+            });
         }
     }
 }
---
+++ Products/Migrations/20201204135639_initialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Products.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true, defaultValue: "Information om produkten saknas..."),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true, defaultValue: "https://i.pinimg.com/originals/21/ff/a1/21ffa154e3d8639299017ab5683e55cc.jpg")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
