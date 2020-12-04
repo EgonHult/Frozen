1071108ceadaa68fd111c6b6d7122381b2449955
Nick Swardh <nickswardh@gmail.com>
2020-12-04 14:09:27 +0100

    Added SQL-Nuget packages and configured startup

---
+++ Products/Models/ProductsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Products.Models
+{
+    public class ProductsModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Details { get; set; }
+        public int Quantity { get; set; }
+        public string Image { get; set; }
+    }
+}
