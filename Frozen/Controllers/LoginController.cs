cc1408f7373336f381e0ba523dfd6c4f08525bb1
Joel Felldin <joelfelldin@hotmail.com>
2020-12-06 16:56:32 +0100

    Added login and register page with a login controller and viewmodels

---
+++ Frozen/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Frozen.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frozen.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult RegisterPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegisterPage(RegisterViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                //var response = GateWay.RegisterNewUser(viewModel);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frozen/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frozen.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string RePassword { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+    }
+}
