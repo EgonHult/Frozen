71a081aff541ee4413c2b1ea3d5ce6182faee85e
VictorJernestrand <56867809+VictorJernestrand@users.noreply.github.com>
2020-12-10 14:26:40 +0100

    db initializer product fix

--- Products/Program.cs
+++ Products/Program.cs
@@ -17,26 +17,6 @@ namespace Products
         {
             CreateHostBuilder(args).Build().Run();
         }
-        //public static void Main(string[] args)
-        //{
-        //    var host = CreateHostBuilder(args).Build();
-
-        //    using (var scope = host.Services.CreateScope())
-        //    {
-        //        var services = scope.ServiceProvider;
-        //        try
-        //        {
-        //            var context = services.GetRequiredService<ProductsDbContext>();
-        //            DbInitializer.Initialize(context);
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            var logger = services.GetRequiredService<ILogger<Program>>();
-        //            logger.LogError(ex, "An error occured while seeding the database");
-        //        }
-        //    }
-        //    host.Run();
-        //}
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
