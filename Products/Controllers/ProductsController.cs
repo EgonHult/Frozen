537219551ce894d163e193ebf604dfb316c53129
Joel Felldin <joelfelldin@hotmail.com>
2020-12-10 14:46:07 +0100

    Added controller to products Microservice

---
+++ Products/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Products.Context;
+using Products.Models;
+using Products.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Products.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductsDbContext _context;
+        private readonly IProductRepository _productsRepository;
+        public ProductsController(ProductsDbContext context, IProductRepository productRepository)
+        {
+            _context = context;
+            this._productsRepository = productRepository;
+        }
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductModel>>> GetProductsAsync()
+        {
+            var result = await _productsRepository.GetAllProducts();
+            return Ok(result);
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
