89f1e231aeaa82c89a0ebf01ba708bb08fa07208
EgonHult <robin_k55@hotmail.com>
2021-01-13 15:08:03 +0100

    Add functions to the +,- and remoce buttons

--- Frozen/wwwroot/js/site.js
+++ Frozen/wwwroot/js/site.js
@@ -10,26 +10,32 @@ function AddToCart(productId) {
             }
         }).then(data => {
             document.getElementById("cartButtonCount").innerHTML = data;
+            IncreaseProductQuantity(productId);
+            CalculateTotalPrice();
         });
 }
 function ReduceFromCart(productId) {
     fetch("https://localhost:44362/cart/reducefromcart/?productId=" + productId)
         .then(cartResponse => {
-            if (cartResponse.Ok) {
+            if (cartResponse.ok) {
                 return cartResponse.text();
             }
         }).then(data => {
             document.getElementById("cartButtonCount").innerHTML = data;
+            DecreaseProductQuantity(productId);
+            CalculateTotalPrice();
         });
 }
 function RemoveFromCart(productId) {
     fetch("https://localhost:44362/cart/removefromcart/?productId=" + productId)
         .then(cartResponse => {
-            if (cartResponse.Ok) {
+            if (cartResponse.ok) {
                 return cartResponse.text();
             }
         }).then(data => {
             document.getElementById("cartButtonCount").innerHTML = data;
+            HideRemovedProduct(productId);
+            CalculateTotalPrice();
         });
 }
 
@@ -42,6 +48,44 @@ function CountProductsInCart() {
         }).then(data => {
             document.getElementById("cartButtonCount").innerHTML = data;
         });
+}
+
+function IncreaseProductQuantity(productId) {
+    let productQuantityElement = document.getElementById('productinCartQuantity_' + productId);
+    productQuantityElement.innerHTML = parseInt(productQuantityElement.innerHTML) + 1;
+   
+}
+
+function DecreaseProductQuantity(productId) {
+    let productQuantityElement = document.getElementById('productinCartQuantity_' + productId);
+    let quantity = parseInt(productQuantityElement.innerHTML) - 1;
+    productQuantityElement.innerHTML = quantity;
+
+    HideProductLessThanOne(productId, quantity);
+   
+}
+
+function HideProductLessThanOne(productId, quantity) {
+    if (quantity < 1) {
+        HideRemovedProduct(productId);
+    }
+    
+}
+
+function HideRemovedProduct(productId) {
+    document.getElementById('productInCart_' + productId).style.display = 'none';
+}
+
+function CalculateTotalPrice() {
+    fetch("https://localhost:44362/cart/calculatetotalprice/")
+        .then(cartResponse => {
+            if (cartResponse.ok) {
+                return cartResponse.text();
+            }
+        }).then(data => {
+            document.getElementById("totalPrice").innerHTML = data;           
+        });
+
 }
 
 CountProductsInCart();
