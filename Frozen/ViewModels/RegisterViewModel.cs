1367c3f8cb3321e7e26bb5e02c209c21b3503976
Nick Swardh <nickswardh@gmail.com>
2021-01-18 14:10:52 +0100

    Updated registerform. All fields are now required

--- Frozen/ViewModels/RegisterViewModel.cs
+++ Frozen/ViewModels/RegisterViewModel.cs
@@ -1,26 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Frozen.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Ange epostadress")]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Ange lösenord")]
         public string Password { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Bekräfta lösenord")]
         public string RePassword { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Ange förnamn")]
         public string FirstName { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Ange efternamn")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Ange adress")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Ange stad")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Ange postnummer")]
         public string Zip { get; set; }
+
+        [Required(ErrorMessage = "Ange telefonummer")]
         public string Phone { get; set; }
     }
 }
